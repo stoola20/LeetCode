363c69688408adb1be1cfd9893931b110d1a0b4c
Hsun <stoola20@icloud.com>
2023-09-03 14:36:01 +0800

    weekly_contest_361: another solution

--- Weekly Contest/weekly_contest_361.playground/Contents.swift
+++ Weekly Contest/weekly_contest_361.playground/Contents.swift
@@ -1,6 +1,35 @@
 import UIKit
 
-class Solution {
+class SolutionA {
+    func countSymmetricIntegers(_ low: Int, _ high: Int) -> Int {
+        var targets = 0
+
+        for num in low ... high {
+            let string = String(num).map { String($0) }
+            let n = string.count
+
+            if n % 2 != 0 {
+                continue
+            }
+
+            var sum1 = 0
+            var sum2 = 0
+
+            for i in 0 ... (n / 2 - 1) {
+                sum1 += Int(string[i])!
+                sum2 += Int(string[n - 1 - i])!
+            }
+
+            if sum1 == sum2 {
+                targets += 1
+            }
+        }
+
+        return targets
+    }
+}
+
+class SolutionB {
     func countSymmetricIntegers(_ low: Int, _ high: Int) -> Int {
         var targets = 0
 
