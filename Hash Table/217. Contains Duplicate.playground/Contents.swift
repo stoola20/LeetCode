2ad0303dbc892c89d30966c037f821252cd09bc9
Hsun <stoola20@icloud.com>
2024-05-23 10:56:15 +0800

    217. Contains Duplicate

---
+++ Hash Table/217. Contains Duplicate.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var numSet: Set<Int> = []
+
+        for num in nums {
+            if numSet.contains(num) {
+                return true
+            }
+
+            numSet.insert(num)
+        }
+
+        return false
+    }
+}
